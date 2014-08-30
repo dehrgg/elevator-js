d6dfa69b2385f63b0b4dd37e58579993e43e9935
Dylan <kdeece@gmail.com>
2014-08-29 23:58:58 -0600

    Simplistic functionality for probability model

--- js/building-settings.js
+++ js/building-settings.js
@@ -2,5 +2,9 @@ app.Models.BuildingSettings = Backbone.Model.extend({
 	defaults: {
 		groundFloors: 3,
 		aboveGroundFloors: 17
+	},
+
+	totalFloors: function() {
+		return this.get('groundFloors') + this.get('aboveGroundFloors');
 	}
 });
--- js/probability.js
+++ js/probability.js
@@ -1,14 +1,54 @@
-app.Models.Probability = Backbone.Model.extend({
+(function() {
+		app.Models.Probability = Backbone.Model.extend({
 
-	overrides: [],
-	
-	getArrivals: function(floor) {
-		var arrivals = {};
-		// arrivals[app.UP] = destination;
-		// arrivals[app.DOWN] = destination;
-	},
+		overrides: [],
+		
+		getArrivals: function(floor) {
+			var arrivals = {}, destination;
+			if (_anyoneArriving(floor, app.UP)) {
+				destination = _randomBetween(floor + 1, this.get('buildingSettings').totalFloors());
+				arrivals[app.UP] = destination;
+			}
+			if (_anyoneArriving(floor, app.DOWN)) {
+				destination = _randomBetween(1, this.get('buildingSettings').get('groundFloors'));
+				arrivals[app.DOWN] = destination;
+			}
+			return arrivals;
+		},
 
-	probabilityForCurrentTime: function(floor, direction) {
-		var buildingSettings = this.get('buildingSettings');
+		defaults: function() { return _residentialDefaults; }
+	});
+
+	function _annyoneArriving(floor, direction) {
+		return Math.random() < _probabilityForCurrentTime(floor, direction);
 	}
-});
+
+	function _probabilityForCurrentTime(floor, direction) {
+		var isGroundFloor = floor <= this.get('buildingSettings').get('groundFloors');
+		if (isGroundFloor && direction === app.UP) {
+			return 0.4;
+		}
+		else if (isGroundFloor) {
+			return (floor === 1) ? 0 : 0.05;
+		}
+		else if (direction === app.DOWN) {
+			return 0.1;
+		}
+		else {
+			return (floor === this.get('buildingSettings').totalFloors()) ? 0 : 0.05;
+		}
+	}
+
+	function _randomBetween(low, high) {
+		return Math.floor(Math.random() * (low - high)) + low;
+	}
+
+	var _residentialDefaults = {
+
+	};
+
+	_officeDefaults = {
+
+	};
+
+})();
