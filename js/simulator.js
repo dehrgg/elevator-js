404d12df36ccf54fc2b507f2fb36eb04c050e5e0
Dylan <kdeece@gmail.com>
2014-09-02 18:47:16 -0600

    Clean up simulator model with respect to elevator and floor changes

--- js/simulator.js
+++ js/simulator.js
@@ -1,48 +1,33 @@
 (function(){
-
-	app.Models.Simulation = Backbone.Model.extend({
+	
+	app.Models.Simulator = Backbone.Model.extend({
 
 		defaults: {
 			processTick: 1,
-			groundLevels: 3,
-			aboveGroundLevels: 17
 		},
 
 		initialize: function() {
-
-			this.set('floors', new app.Collections.Floors());
-			this.set('elevators', new app.Collections.Elevators());
-
-			for (var i = 1; i <= this.get('stories'); ++i) {
-				var floor = new app.Models.Floor({
-					floor: i
-				});
-				this.listenTo(floor, 'arrival', this.newArrival);
-				this.get('floors').add(floor);
-			}
-			for (i = 0; i < this.get('elevatorCount'); ++i) {
-				var elevator = new app.Models.Elevator({
-					currentFloor: 0
-				});
-				this.get('elevators').add(elevator);
-			}
-
+			this.listenTo(this.get('floors'), 'arrival', this.dispatch);
+			this.listenTo(this.get('elevators'), 'change:currentFloor', this.elevatorArrival);
 		},
 
-		newArrival: function(floor, direction) {
-			var destination;
-			if (direction === app.DOWN) {
-				destination = Math.floor(Math.random() * this.get('groundLevels'));
+		elevatorArrival: function(elevator, floorNumber) {
+			var floor = this.get('floors').at(floorNumber - 1),
+				direction = elevator.get('direction');
+			if (floor.hasPeopleWaiting(direction)) {
+				while (elevator.hasSpace()) {
+					elevator.addPassenger(floor.nextInLine(direction))
+				}
 			}
-			else {
-				destination = Math.floor(Math.random() * this.get('aboveGroundLevels')) + this.get('groundLevels');
-			}
+			this.dispatch();
+		},
+
+		dispatch: function() {
+			this.get('decisionModel').dispatch(this.get('floors'), this.get('elevators'));
 		},
 
 		tick: function() {
-			this.get('floors').each(function(floor){
-				floor.checkArrivals();
-			});
+			this.get('probability').checkArrivals(this.get('floors'));
 		},
 
 		start: function() {
