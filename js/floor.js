bb3d3d08cc079406d6078aca172474fe6f30ce1e
Dylan <kdeece@gmail.com>
2014-09-02 18:45:43 -0600

    Relocate arrival logic from the floor model, bubble arrival events

--- js/floor.js
+++ js/floor.js
@@ -1,34 +1,30 @@
 window.app.Models.Floor = Backbone.Model.extend({
 
 	initialize: function() {
-		this.set('goingUp', new app.Collections.PersonQueue());
-		this.set('goingDown', new app.Collections.PersonQueue());
+		var up = new app.Collections.PersonQueue();
+		var down = new app.Collections.PersonQueue();
+		this.set('goingUp', up);
+		this.set('goingDown', down);
+		this.listenTo(up, 'add', this.arrival);
+		this.listenTo(down, 'add', this.arrival);
 	},
 
-	checkArrivals: function() {
-		var destination;
-
-		if (Math.random() < this.get('probabilityUp')) {
-			this.get('goingUp').add(new Person({
-				destination: app.getDestination()
-			}));
-			this.trigger('arrival', this, app.UP);
-		}
-		if (Math.random() < this.get('probabilityDown')) {
-			this.trigger('arrival', this, app.DOWN);
-		}
+	hasPeopleWaiting: function(direction) {
+		return ( direction === app.UP && this.get('goingUp').length > 0) 
+			|| (direction === app.DOWN && this.get('goingDown').length > 0);
 	},
 
-	nextInLine: function() {
-		return this.get('queue').shift();
+	nextInLine: function(direction) {
+		var key = (direction === app.UP) ? 'goingUp' : 'goingDown';
+		this.get(key).shift();
 	},
 
-	elevatorArrival: function(elevator) {
-		// fill it up
+	arrival: function() {
+		this.trigger('arrival');
 	}
 });
 
-window.app.Collections.Floors = Backbone.Model.extend({
+app.Collections.Floors = Backbone.Collection.extend({
 	model: app.Models.Floor,
-	comparator: floor
+	comparator: 'floor'
 });
